b1e62fceb2b407fff2f83305620497c43380c4af
sekiro <85436007@qq.com>
2026-06-13 00:59:10 +0800

    移除首页排序控件

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,6 @@ import ScheduleView from "./components/ScheduleView.vue";
 import LineupDialog from "./components/LineupDialog.vue";
 import CompareDialog from "./components/CompareDialog.vue";
 
-const sortOptions = [
-  ["rank", "综合排名"],
-  ["squadQuality", "阵容质量"],
-  ["recentMatchRating", "近期比赛"],
-  ["positionalBalance", "位置平衡"],
-  ["squadDepth", "阵容深度"],
-  ["cohesionContinuity", "协同连续性"],
-  ["ageLoad", "年龄耐久"]
-];
-
 function setView(view) {
   appState.view = view;
   if (view === "schedule" && !appState.liveScheduleAttempted) {
@@ -48,15 +38,6 @@ onMounted(() => {
     </header>
 
     <template v-if="appState.view !== 'schedule'">
-      <section class="controls" aria-label="筛选与排序">
-        <label class="select-box">
-          <span>排序</span>
-          <select v-model="appState.sortMode">
-            <option v-for="[value, label] in sortOptions" :key="value" :value="value">{{ label }}</option>
-          </select>
-        </label>
-      </section>
-
       <LiveBanner />
 
       <main class="workspace">
